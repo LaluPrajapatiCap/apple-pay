ecac31e25a675026ea9ff7a3d820f69b523549b0
Prajapati <lalu.prajapati@capgemini.com>
2022-09-15 19:40:34 +0530

    Updating default routs

--- server.js
+++ server.js
@@ -13,9 +13,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.send('I am starting my development')
+// });
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.render(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
   res.send('I am starting my development')
 });
+}
 
 // app.post('/payment', (req,res) => {
 //   const { product, token } = req.body;
@@ -58,14 +70,6 @@ app.post('/create-payment-intent', async (req, res) => {
     res.status(400).json({ error: { message: e.message}})
   }
 })
-
-// serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
